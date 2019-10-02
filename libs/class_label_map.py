30f9064f9ce76042cacd0167fda936e63b6e503d
yiskw <38214459+yiskw713@users.noreply.github.com>
2019-10-02 14:02:19 +0900

    Update class_label_map.py

--- libs/class_label_map.py
+++ libs/class_label_map.py
@@ -1,4 +1,4 @@
-def get_id2label_map():
+def get_cls2id_map():
     cls2id_map = {
         'daisy': 0, 'dandelion': 1, 'rose': 2, 'sunflower': 3, 'tulip': 4
     }
@@ -6,6 +6,6 @@ def get_id2label_map():
     return cls2id_map
 
 
-def get_label2id_map():
-    cls2id_map = get_id2label_map()
+def get_id2cls_map():
+    cls2id_map = get_cls2id_map()
     return {val: key for key, val in cls2id_map.items()}
